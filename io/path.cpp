3067a2472dae3f8a833a2c59e91c06142862109b
korenkonder <admin@mysterious-home.net>
2024-11-17 18:28:44 +0300

    Synced changes from ReDIVA

--- io/path.cpp
+++ io/path.cpp
@@ -6,6 +6,7 @@
 #include "path.hpp"
 #include "../str_utils.hpp"
 #include "file_stream.hpp"
+#include <fstream>
 
 bool path_check_path_exists(const char* path) {
     wchar_t* path_temp = utf8_to_utf16(path);
@@ -445,22 +446,24 @@ bool path_create_file(const wchar_t* path) {
 }
 
 bool path_create_directory(const char* path) {
-    const char* _path = path;
+    wchar_t* path_temp = utf8_to_utf16(path);
+    const wchar_t* _path = path_temp;
     while (true) {
-        const char* c = strchr(_path, '\\');
+        const wchar_t* c = wcschr(_path, L'\\');
         if (!c)
-            c = strchr(_path, '/');
+            c = wcschr(_path, L'/');
 
         if (!c)
             break;
 
         _path = c + 1;
-        std::wstring temp = utf8_to_utf16(std::string(path, c - path));
-        if (!path_check_directory_exists(temp.c_str()) && !CreateDirectoryW(temp.c_str(), 0))
+        std::wstring temp(path_temp, c - path_temp);
+        if (!path_check_directory_exists(temp.c_str()) && !CreateDirectoryW(temp.c_str(), 0)) {
+            free_def(path_temp);
             return false;
+        }
     }
 
-    wchar_t* path_temp = utf8_to_utf16(path);
     bool ret = path_check_directory_exists(path_temp) || CreateDirectoryW(path_temp, 0);
     free_def(path_temp);
     return ret;
@@ -533,15 +536,175 @@ bool path_delete_directory(const wchar_t* path) {
     return !path_check_directory_exists(path) || RemoveDirectoryW(path);
 }
 
-bool path_rename_file(const char* old_path, const char* new_path) {
+bool path_copy_file(const char* src, const char* dst) {
+    if (!path_check_file_exists(src)
+        || path_check_directory_exists(dst))
+        return false;
+
+    wchar_t* src_temp = utf8_to_utf16(src);
+    wchar_t* dst_temp = utf8_to_utf16(dst);
+    std::ifstream ifs(src_temp, std::ios::in | std::ios::binary);
+    std::ofstream ofs(dst_temp, std::ios::out | std::ios::trunc | std::ios::binary);
+    free_def(src_temp);
+    free_def(dst_temp);
+
+    if (!ifs.is_open() || !ofs.is_open())
+        return false;
+
+    ofs << ifs.rdbuf();
+
+    ifs.close();
+    ofs.close();
+    return true;
+}
+
+bool path_copy_file(const wchar_t* src, const wchar_t* dst) {
+    if (!path_check_file_exists(src)
+        || path_check_directory_exists(dst))
+        return false;
+
+    std::ifstream ifs(src, std::ios::in | std::ios::binary);
+    std::ofstream ofs(dst, std::ios::out | std::ios::trunc | std::ios::binary);
+
+    if (!ifs.is_open() || !ofs.is_open())
+        return false;
+
+    ofs << ifs.rdbuf();
+
+    ifs.close();
+    ofs.close();
+    return true;
+}
+
+int32_t path_compare_files(const char* path_1, const char* path_2) {
+    wchar_t* path_1_temp = utf8_to_utf16(path_1);
+    wchar_t* path_2_temp = utf8_to_utf16(path_2);
+
+    int32_t ret;
+    if (!path_check_path_exists(path_1_temp))
+        ret = -1;
+    else if (!path_check_path_exists(path_2_temp))
+        ret = 1;
+    else if (!path_check_file_exists(path_1_temp) || !path_check_file_exists(path_2_temp))
+        ret = -1;
+    else {
+        WIN32_FILE_ATTRIBUTE_DATA fad_1;
+        WIN32_FILE_ATTRIBUTE_DATA fad_2;
+        GetFileAttributesExW(path_1_temp, GetFileExInfoStandard, &fad_1);
+        GetFileAttributesExW(path_2_temp, GetFileExInfoStandard, &fad_2);
+
+        LARGE_INTEGER size_1;
+        size_1.HighPart = fad_1.nFileSizeHigh;
+        size_1.LowPart = fad_1.nFileSizeLow;
+
+        LARGE_INTEGER size_2;
+        size_2.HighPart = fad_2.nFileSizeHigh;
+        size_2.LowPart = fad_2.nFileSizeLow;
+
+        if (size_1.QuadPart == size_2.QuadPart) {
+            ret = 0;
+
+            std::ifstream ifs_1(path_1_temp, std::ios::in | std::ios::binary);
+            std::ifstream ifs_2(path_2_temp, std::ios::in | std::ios::binary);
+            while (ifs_1.good() && ifs_2.good()) {
+                char buf_1[0x200];
+                char buf_2[0x200];
+                ifs_1.read(buf_1, sizeof(buf_1));
+                ifs_2.read(buf_2, sizeof(buf_2));
+                if (ifs_1.gcount() != ifs_2.gcount()) {
+                    ret = -1;
+                    break;
+                }
+                else if (memcmp(buf_1, buf_2, ifs_1.gcount())) {
+                    ret = 1;
+                    break;
+                }
+            }
+        }
+        else
+            ret = 1;
+    }
+
+    free_def(path_1_temp);
+    free_def(path_2_temp);
+    return ret;
+}
+
+int32_t path_compare_files(const wchar_t* path_1, const wchar_t* path_2) {
+    int32_t ret;
+    if (!path_check_path_exists(path_1))
+        ret = -1;
+    else if (!path_check_path_exists(path_2))
+        ret = 1;
+    else if (!path_check_file_exists(path_1) || !path_check_file_exists(path_2))
+        ret = -1;
+    else {
+        WIN32_FILE_ATTRIBUTE_DATA fad_1;
+        WIN32_FILE_ATTRIBUTE_DATA fad_2;
+        GetFileAttributesExW(path_1, GetFileExInfoStandard, &fad_1);
+        GetFileAttributesExW(path_2, GetFileExInfoStandard, &fad_2);
+
+        LARGE_INTEGER size_1;
+        size_1.HighPart = fad_1.nFileSizeHigh;
+        size_1.LowPart = fad_1.nFileSizeLow;
+
+        LARGE_INTEGER size_2;
+        size_2.HighPart = fad_2.nFileSizeHigh;
+        size_2.LowPart = fad_2.nFileSizeLow;
+
+        if (size_1.QuadPart == size_2.QuadPart) {
+            ret = 0;
+
+            std::ifstream ifs_1(path_1, std::ios::in | std::ios::binary);
+            std::ifstream ifs_2(path_2, std::ios::in | std::ios::binary);
+            while (ifs_1.good() && ifs_2.good()) {
+                char buf_1[0x200];
+                char buf_2[0x200];
+                ifs_1.read(buf_1, sizeof(buf_1));
+                ifs_2.read(buf_2, sizeof(buf_2));
+                if (ifs_1.gcount() != ifs_2.gcount()) {
+                    ret = -1;
+                    break;
+                }
+                else if (memcmp(buf_1, buf_2, ifs_1.gcount())) {
+                    ret = 1;
+                    break;
+                }
+            }
+        }
+        else
+            ret = 1;
+    }
+    return ret;
+}
+
+bool path_fs_copy_file(const char* src, const char* dst) {
+    std::string _dst(dst);
+    _dst.append(".fs_copy_file.tmp");
+    if (path_copy_file(src, _dst.c_str()))
+        return path_move_file(_dst.c_str(), dst);
+    return false;
+}
+
+bool path_fs_copy_file(const wchar_t* src, const wchar_t* dst) {
+    std::wstring _dst(dst);
+    _dst.append(L".fs_copy_file.tmp");
+    if (path_copy_file(src, _dst.c_str()))
+        return path_move_file(_dst.c_str(), dst);
+    return false;
+}
+
+bool path_move_file(const char* old_path, const char* new_path) {
     wchar_t* old_path_temp = utf8_to_utf16(old_path);
     wchar_t* new_path_temp = utf8_to_utf16(new_path);
-    bool ret = !_wrename(old_path_temp, new_path_temp);
+    path_delete_file(new_path_temp);
+    bool ret = MoveFileW(old_path_temp, new_path_temp);
     free_def(old_path_temp);
     free_def(new_path_temp);
     return ret;
 }
 
-bool path_rename_file(const wchar_t* old_path, const wchar_t* new_path) {
-    return !_wrename(old_path, new_path);
+bool path_move_file(const wchar_t* old_path, const wchar_t* new_path) {
+    path_delete_file(new_path);
+    return MoveFileW(old_path, new_path);
 }
